0e4c7ce15368a11db3966dca77d0c077b95c8025
Sunghyun Yoo <optid77@gmail.com>
2022-07-27 21:20:06 +0900

    add notion-to-md
    .

--- notion2blog/main.ts
+++ notion2blog/main.ts
@@ -4,9 +4,8 @@ import { getNotionPages } from "./notion2md"
 const main = async () => {
   // Notion에서 NotionPage Object 불러오기
   const notionPages = await getNotionPages()
-  makeBlogPosts(notionPages)
-
   // NotionPage Object로 Category별로 폴더 생성 및 .md 파일 생성
+  makeBlogPosts(notionPages)
 }
 
 main()
